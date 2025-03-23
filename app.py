a4ca4de68a599ff289bd92b6bb2746ac4b6123a2
Mariyyah Alrasheed <mariyyah.alrasheed@gmail.com>
2025-03-23 15:00:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -231,4 +231,5 @@ def serve_image(company, image_name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
+
